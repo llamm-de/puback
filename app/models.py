425ed20623651ece11e80ead68cb031424d4bd51
llamm-de <lamm.lukas@googlemail.com>
2022-09-10 09:19:20 +0200

    relatioinships implemented in models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,20 @@
 from . import db
 
+applicants = db.Table('applicants',
+    db.Column('person_id', db.Integer, db.ForeignKey('person.id'), primary_key=True),
+    db.Column('project_id', db.String(20), db.ForeignKey('project.id'), primary_key=True)
+)
+
+
 class Person(db.Model):
     __tablename__ = 'persons'
-    firstname = db.Column(db.String(30), primary_key=True)
-    lastname = db.Column(db.String(30), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(30))
+    lastname = db.Column(db.String(30))
 
     def __repr__(self) -> str:
-        return f"<Person {self.firstname} {self.lastname}>"
+        return f"<Person {self.firstname} {self.lastname} at {id(self)}>"
+
 
 class Publication(db.Model):
     __tablename__ = 'publications'
@@ -16,7 +24,8 @@ class Publication(db.Model):
     doi = db.Column(db.String(50))
 
     def __repr__(self) -> str:
-        return f"<Publication {self.id}>"
+        return f"<Publication {self.id} at {id(self)}>"
+
 
 class Project(db.Model):
     __tablename__ = 'projects'
@@ -27,4 +36,24 @@ class Project(db.Model):
     billing_onject = db.Column(db.Integer)
 
     def __repr__(self) -> str:
-        return f"<Project {self.id}: {self.short_title}>"
+        return f"<Project {self.id}: {self.short_title} at {id(self)}>"
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    username = db.Column(db.String(30), primary_key=True)
+    # To Do: Passhash storage
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<User {self.username} at {id(self)}>"
+
+
+class Role(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self) -> str:
+        return f"<Role {self.username} at {id(self)}>"
